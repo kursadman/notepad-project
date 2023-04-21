250f07856af37f7b561cfcc3276a44d06fd1cd5e
kursad caliskan <kursadcaliskan@gmail.com>
2023-04-21 18:44:31 -0400

    PyQt6 notepad with python v1.0

---
+++ main.py
@@ -0,0 +1,71 @@
+from PyQt6.QtWidgets import QMainWindow, QApplication,QFileDialog,QMessageBox
+import sys
+from NotePad import Ui_MainWindow
+from PyQt6.QtPrintSupport import QPrinter,QPrintDialog
+class NotePadWindow(QMainWindow,Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.show()
+        self.actionSave.triggered.connect(self.saveFile)
+        self.actionNew.triggered.connect(self.fileNew)
+        self.actionOpen.triggered.connect(self.openFile)
+        self.actionPrint.triggered.connect(self.printFile)
+
+        self.actionExit.triggered.connect(self.exitApp)
+        self.actionCopy.triggered.connect(self.textEdit.copy)
+        self.actionCut.triggered.connect(self.textEdit.cut)
+        self.actionPaste.triggered.connect(self.textEdit.paste)
+        self.actionAbout_app.triggered.connect(self.about)
+    def isSaved(self):
+        if not self.textEdit.document().isModified():
+            return True
+
+        ret = QMessageBox.warning(self,"Application","The document has been modified.\n Do you want to save ?",
+        QMessageBox.StandardButton.Save | QMessageBox.StandardButton.Discard)
+
+        if ret == QMessageBox.StandardButton.Save:
+            return self.saveFile()
+        if ret == QMessageBox.StandardButton.Cancel:
+            return False
+        return True
+
+    def saveFile(self):
+        filename = QFileDialog.getSaveFileName(self,"save File")
+        if filename[0]:
+            f = open(filename[0],'w')\
+
+            with f:
+                text = self.textEdit.toPlainText()
+                f.write(text)
+                QMessageBox.about(self,"save file","file has been saved")
+
+
+    def fileNew(self):
+        if self.isSaved():
+            self.textEdit.clear()
+
+    def openFile(self):
+        fName = QFileDialog.getOpenFileName(self,"Open File")
+        if fName[0]:
+            f = open(fName[0],'r')
+
+            with f:
+                data = f.read()
+                self.textEdit.setText(data)
+
+    def printFile(self):
+        printer = QPrinter(QPrinter.PrinterMode.HighResolution)
+        dialog = QPrintDialog(printer)
+        if dialog.exec() == QPrintDialog.DialogCode.Accepted:
+            self.textEdit.print(printer)
+    def exitApp(self):
+        self.close()
+
+    def about(self):
+        QMessageBox.about(self,"About app","This is simple notepad app with PyQt6")
+
+app = QApplication(sys.argv)
+note = NotePadWindow()
+
+sys.exit(app.exec())
